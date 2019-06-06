25985709db25fdd449e09f520cefb65cb9339fbc
Kaz Gofron <kgofron@comcast.net>
2019-06-06 12:38:23 -0400

    Update css_imagej.sh

--- macro/css_imagej.sh
+++ macro/css_imagej.sh
@@ -1,3 +1,9 @@
+#
+# Bash script that allows NSLS2 facility wide starting ImageJ from CSS screens with prepopulated PV for specific EPICS areaDetector.
+#
+# Author: Kazimierz Gofron
+#
+
 export EPICS_CA_MAX_ARRAY_BYTES=60000000
 cd ~
 rm EPICS_AD_Viewer.properties
