fb25020e7d092d8cff4748be2684530fee0598ff
Kaz Gofron <kgofron@comcast.net>
2019-06-06 12:42:08 -0400

    Update css_imagej_pva.sh

--- macro/css_imagej_pva.sh
+++ macro/css_imagej_pva.sh
@@ -1,3 +1,7 @@
+# Bash script that allows NSLS2 facility wide starting ImageJ from CSS screens with prepopulated PV access for specific EPICS areaDetector.
+#
+# Author: Kazimierz Gofron
+#
 cd ~
 rm EPICS_NTNDA_Viewer.properties
 echo "channelName=$1" > EPICS_NTNDA_Viewer.properties
